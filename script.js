6f23d230555f2b6b47f6a0942f1acc01505387c9
Paolo Debuque <pdebuque@gmail.com>
2022-10-27 11:36:43 -0500

    created attack functions and render function

--- script.js
+++ script.js
@@ -5,14 +5,48 @@ $(document).ready(onReady);
 // Example:
 // let fungusHP = 100;
 
+let fungusHP = 100;
+let heroAP = 100;
+
+
 function onReady() {
-    
+
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
-    
-    
+
+    $('.arcane-sceptre').on('click', arcaneSceptre);
+    $('.entangle').on('click', entangle);
+    $('.dragon-blade').on('click', dragonBlade);
+    $('.star-fire').on('click', starFire);
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+}
+
+
+function arcaneSceptre() {
+    fungusHP -= 14;
+    heroAP -= 12;
+    render();
+}
+function entangle() {
+    fungusHP -= 9;
+    heroAP -= 23;
+    render();
+}
+function dragonBlade() {
+    fungusHP -= 47;
+    heroAP -= 38;
+    render();
+}
+function starFire() {
+    fungusHP -= 25;
+    heroAP -= 33;
+    render();
+}
+
+function render() {
+    $('.ap-text').html(heroAP);
+    $('.hp-text').html(fungusHP);
 }
