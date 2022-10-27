f55a0eca51743ad7b7379543331f5f3e3be0ae5a
Paolo Debuque <pdebuque@gmail.com>
2022-10-27 13:04:17 -0500

    refactored into a single handle attack function

--- script.js
+++ script.js
@@ -9,15 +9,37 @@ let fungusHP = 100;
 let heroAP = 100;
 let regenerate = false;
 
+const attacks = {
+    arcaneSceptre: {
+        cost: 12,
+        damage: 14
+    },
+    entangle: {
+        cost: 23,
+        damage: 9
+    },
+    dragonBlade: {
+        cost: 38,
+        damage: 47
+    },
+    starFire: {
+        cost: 33,
+        damage: 25
+    }
+
+}
+
 function onReady() {
 
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
 
-    $('.arcane-sceptre').on('click', arcaneSceptre);
-    $('.entangle').on('click', entangle);
-    $('.dragon-blade').on('click', dragonBlade);
-    $('.star-fire').on('click', starFire);
+    $('.attack-btn').on('click', handleAttack);
+
+    // $('.arcane-sceptre').on('click', arcaneSceptre);
+    // $('.entangle').on('click', entangle);
+    // $('.dragon-blade').on('click', dragonBlade);
+    // $('.star-fire').on('click', starFire);
 
     // 🧠 Remember
     // - Handle events that ->
@@ -25,27 +47,36 @@ function onReady() {
     // - Rendered to the DOM
 }
 
+function handleAttack() {
+    let attack = $(this).attr('class').split(' ')[1];
+    let attackVals = attacks[attack];
+    fungusHP -= attackVals.damage;
+    heroAP -= attackVals.cost;
+    render();
 
-function arcaneSceptre() {
-    fungusHP -= 14;
-    heroAP -= 12;
-    render();
 }
-function entangle() {
-    fungusHP -= 9;
-    heroAP -= 23;
-    render();
-}
-function dragonBlade() {
-    fungusHP -= 47;
-    heroAP -= 38;
-    render();
-}
-function starFire() {
-    fungusHP -= 25;
-    heroAP -= 33;
-    render();
-}
+
+// function arcaneSceptre() {
+//      fungusHP -= 14;
+//      heroAP -= 12;
+//      render();
+// }
+// }
+// function entangle() {
+//     fungusHP -= 9;
+//     heroAP -= 23;
+//     render();
+// }
+// function dragonBlade() {
+//     fungusHP -= 47;
+//     heroAP -= 38;
+//     render();
+// }
+// function starFire() {
+//     fungusHP -= 25;
+//     heroAP -= 33;
+//     render();
+// }
 
 function render() {
     $('.ap-text').html(heroAP);
